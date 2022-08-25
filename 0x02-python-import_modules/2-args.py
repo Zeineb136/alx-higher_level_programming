82277f2806411d8b074a7baa22b3ea4feea8fc98
Zeineb136 <moussa.zeineb1@gmail.com>
2022-08-25 17:44:03 +0000

    python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import sys 
+if __name__ == '__main__':
+    num = len(sys.argv) - 1
+    print(num, " arguments")
+    for i in range(1, num + 1):
+        print("{} : {}".format(i, sys.argv[i]))
