d6ed8013c5308222842b1699e65832e4e2bbac82
Zeineb136 <104872308+Zeineb136@users.noreply.github.com>
2022-08-25 18:54:25 +0100

    python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,6 @@
 import sys
 if __name__ == '__main__':
     num = len(sys.argv) - 1
-    print(num, " argument:")
+    print(num, " arguments:")
     for i in range(1, num + 1):
         print("{} : {}".format(i, sys.argv[i]))
