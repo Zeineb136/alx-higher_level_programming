2e282e0ae562eb08970758408889ec0f58ec9703
Zeineb136 <moussa.zeineb1@gmail.com>
2022-08-29 22:48:50 +0000

    python

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) < 1:
+        return None
+    else:
+        max1 = 0
+        for i in range(len(my_list)):
+            a = my_list[i]
+            print(a)
+            if a > max1:
+               max1 = a
+            else: 
+               max1 = max1
+    return(max1)
