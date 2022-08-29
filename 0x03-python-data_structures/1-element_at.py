8f724a1a4674af91a0e3350f31039bd80dd18c3a
Zeineb136 <moussa.zeineb1@gmail.com>
2022-08-29 12:36:51 +0000

    python

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    max_idx = len(my_list) - 1
     if idx < 0:
         return(0)
-    elif idx > len(my_list):
+    elif idx > max_idx:
         return(0)
     else:
         return(my_list[idx])
