db04e2bc8ec173f087186acc3d0a1e1ed31fe115
Zeineb136 <104872308+Zeineb136@users.noreply.github.com>
2022-08-30 01:17:15 +0100

    python

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-<<<<<<< HEAD
-       if idx < 0 or idx >= len(my_list)
-          return(my_list)
-
-       del my_list[idx]
-       return(my_list)
-=======
-    idx = 3
-    del my_list[idx]
-    return(my_list)
->>>>>>> 2c9d19eba620eae7569eb8c6ad78dc27e69c4092
+    if idx < 0 or idx >= len(my_list)
+        return(my_list)
+        
+        del my_list[idx]
+        return(my_list)
