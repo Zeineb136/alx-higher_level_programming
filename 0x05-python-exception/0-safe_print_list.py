3c3471cfa407afc243ed0b1a84585c5a1c95ed80
Zeineb136 <moussa.zeineb1@gmail.com>
2022-09-12 17:37:20 +0000

    python

--- 0x05-python-exception/0-safe_print_list.py
+++ 0x05-python-exception/0-safe_print_list.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+	i = 0
+	try:
+		for x in my_list:
+			print(x, end='')
+			i += 1
+	except IndexError:
+		break
+
+	print('')
+	return i	
+	
+  		
+
+		 
+                    
+	
