0da2c1f5a394f17cb8df6a86e769b4a87d9513b9
Zeineb136 <104872308+Zeineb136@users.noreply.github.com>
2022-09-14 00:52:55 +0100

    python

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,22 +8,21 @@ class Square:
         size     size of the square
     """
     def __init__(self, size=0):
-    	"""initialize  the Square
-	Args:
-	   size     size of the square	
-	Returns:
-	   None
-	"""
-	self.__size = size
+        """initialize  the Square
+        Args:
+        size    size of the square
+        Returns:
+            None
+        """
+    self.__size = size
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
 
-	if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
     def area(self):
-    	"""calculates the area of the square 
-        
-	Returns:
-		the area of the square
-     	"""
-    	return(self.__size**2)
+    	"""calculates the area of the square
+        Returns:
+            the area of the square
+        """
+        return(self.__size**2)
