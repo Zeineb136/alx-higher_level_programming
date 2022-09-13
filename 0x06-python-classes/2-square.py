40bff5c942bb731cfaf9c54d8061917cfffa4198
Zeineb136 <104872308+Zeineb136@users.noreply.github.com>
2022-09-13 23:44:10 +0100

    python

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,11 +5,11 @@
 class Square:
     """size is a Private instance attribute
     Attributes:
-        __size	 size of the square
+        __size     size of the square
     """
     def __init__(self, size=0):
         self.__size = size
         if type(size) is not int:
-        	raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
-        	raise ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
