b168315327a480894941f6967a9b0193c69e3fef
Zeineb136 <moussa.zeineb1@gmail.com>
2022-09-14 00:39:46 +0000

    python

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,24 +5,14 @@
 class Square:
     """size is a Private instance attribute
     Attributes:
-        size     size of the square
+        __size: size of the square
     """
     def __init__(self, size=0):
-        """initialize  the Square
-        Args:
-        size    size of the square
-        Returns:
-            None
-        """
-    self.__size = size
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
+    	self.__size = size
+    	if type(size) is not int:
+        	raise TypeError("size must be an integer")
+    	if size < 0:
+        	raise ValueError("size must be >= 0")
 
     def area(self):
-        """calculates the area of the square
-        Returns:
-            the area of the square
-        """
-        return self.__size**2
+        return (self.__size)**2
