a41e1321cab63d38a3b10487fe10ca7e6a0eb172
Zeineb136 <moussa.zeineb1@gmail.com>
2022-09-19 20:54:40 +0000

    python

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""class that defines a rectangle"""
+
+
+class Rectangle:
+    """rectangle class"""
+    def __init__(self, width=0, height=0):
+	"""initialize the Rectangle
+	Args:
+        width   width of the rectangle
+        height  height of the rectangle
+        Returns: 
+            None
+        """
+        self.__width = width
+        self.__height = height  
+
+def width(self):
+def width(self, value):
+
+def height(self):
+def height(self, value):
+
+
