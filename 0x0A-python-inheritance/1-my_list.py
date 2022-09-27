1bffa5564b7387a7f059e6dfdf617f92a6681692
Zeineb136 <moussa.zeineb1@gmail.com>
2022-09-27 02:30:52 +0000

    python

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+MyList class
+"""
+
+
+class MyList(list):
+    """class that contains list"""
+
+
+    def print_sorted(self):i
+	"""prints the list, but sorted (ascending sort)"""
+	print(sorted(self))
+
