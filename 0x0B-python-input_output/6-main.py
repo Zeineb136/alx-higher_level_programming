00ccf52e8b79c01448b05fb4363759c8e188abfe
Zeineb136 <104872308+Zeineb136@users.noreply.github.com>
2022-09-28 03:17:25 +0100

    python

--- 0x0B-python-input_output/6-main.py
+++ 0x0B-python-input_output/6-main.py
@@ -1,7 +1,5 @@
-{"is_active": true, 12 }
-guillaume@ubuntu:~/0x0B$ cat 6-main.py
 #!/usr/bin/python3
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+/load_from_json_file = __import__('6-load_from_json_file').load_from_json_file*
 
 filename = "my_list.json"
 my_list = load_from_json_file(filename)
