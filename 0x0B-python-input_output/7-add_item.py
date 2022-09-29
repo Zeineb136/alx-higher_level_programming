6d6e14455d074ff8f3c9d03eb4cd588c8603722e
Zeineb136 <104872308+Zeineb136@users.noreply.github.com>
2022-09-29 03:26:58 +0100

    python

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""
-script that adds all arguments to a Python list, and then save them to a file
-"""
-
-
 import sys
 import json
+
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
+"""script that adds all arguments to a Python list, and then save them to a file"""
 try:
     list_python = load_from_json_file("add_item.json")
 except:
